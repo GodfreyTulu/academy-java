c9db5a826d76333ca5207a2c9371a7ce2a24d2c5
(Tulu) Godfrey Tulu <tulugodfrey@gmail.com>
2024-09-26 12:27:01 -0230

    Hello and Self-led Packages added

---
+++ src/main/java/com/bptn/course/_06_self_led/RandomNumbers.java
@@ -0,0 +1,13 @@
+package com.bptn.course._06_self_led;
+
+public class RandomNumbers {
+
+	public static void main(String[] args) {
+		
+		int randomNumber = (int)(Math.random() * 1001);
+		
+		System.out.println("After execution the Random Number is: " + randomNumber);	
+		
+	}
+
+}
